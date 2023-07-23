f39d446fe7975be4dda09dde4c36508528844aaa
Chetanlambe <138874931+Chetanlambe@users.noreply.github.com>
2023-07-23 11:37:38 +0530

    solid rectangle
    To draw the solid rectangle

---
+++ solidrectangle.java
@@ -0,0 +1,18 @@
+//print solid rectange
+import java.util.Scanner;
+public class solidrectangle {
+    public static void main(String[] args) {
+        int n,i,m,j;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter th number of row");
+        n = sc.nextInt();// n is denoted by the row
+        System.out.println("enter the number of column");
+        m = sc.nextInt();
+        for(i=1;i<=n;i++){
+            for(j=1;j<=m;j++){
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+    }
+}

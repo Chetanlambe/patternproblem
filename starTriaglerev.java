5df686b56acc1d7e5c62d762143e12bc9bd69779
Chetan Lambe <138874931+Chetanlambe@users.noreply.github.com>
2023-07-28 18:11:32 +0530

    starTriagle reverse
    Draw the reverse star triagle

---
+++ starTriaglerev.java
@@ -0,0 +1,25 @@
+/*
+    *
+   **
+  ***
+ ****
+ Draw the above pattern
+ */
+import java.util.Scanner;
+public class starTriaglerev {
+    public static void main(String[] args) {
+        int i,j,k,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the number of row");
+        n = sc.nextInt();
+        for(i=1;i<=n;i++){
+            for(j=1;j<=n-i;j++){
+                System.out.print(" ");
+            }
+            for(k=1;k<=i;k++){
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
